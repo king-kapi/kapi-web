e9bc5d20f2712c3bf6eb83c935d740017ea0903d
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-01-28 23:32:45 -0800

    fix: remove numerical enum

--- src/models/PermissionLevel.ts
+++ src/models/PermissionLevel.ts
@@ -1,6 +1,7 @@
 enum PermissionLevel {
-    ADMIN,
-    GENERAL
-}
+    ADMIN = "ADMIN",
+    GENERAL = "GENERAL",
+} // Do not use plain numerical enums, they are known to be problematic
+//@see https://blog.logrocket.com/why-typescript-enums-suck/
 
 export default PermissionLevel;
