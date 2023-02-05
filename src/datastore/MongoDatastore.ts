c2f2953bcc0be85d7f76a33d0456ff5727b2654c
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-02-04 21:04:15 -0800

    fix: bad eslint rule, concise singleton implementation
    the "plugin:@typescript-eslint/recommended-requiring-type-checking" plugin is unable to reason about default exports

--- src/datastore/MongoDatastore.ts
+++ src/datastore/MongoDatastore.ts
@@ -1,42 +1,40 @@
-import { Collection, Db, MongoClient } from "mongodb";
-import User, { newUser } from "../models/User";
-import Collections from "./Collections";
-import UserCollection from "./UserCollection";
+import { Collection, Db, MongoClient } from 'mongodb'
+import Collections from './Collections'
+import UserCollection from './UserCollection'
 
 class MongoDatastore {
-    private static instance: MongoDatastore;
-    private client: MongoClient;
-    private datastore: Db;
-    public users: UserCollection;
-    private chatsCol: Collection;
-    private messagesCol: Collection;
+  private static instance: MongoDatastore
+  private datastore: Db = this.client.db(process.env.MONGO_DB_NAME ?? 'designthriving')
+  public users: UserCollection = new UserCollection(this.datastore.collection(Collections.USERS))
+  // TODO: Add type parameter to the following fields (Collection<?>)
+  private chatsCol: Collection = this.datastore.collection(Collections.CHATS)
+  private messagesCol: Collection = this.datastore.collection(Collections.MESSAGES)
 
-    constructor(client: MongoClient) {
-        this.client = client;
-        this.datastore = this.client.db(process.env.MONGO_DB_NAME || "designthriving");
-        this.users = new UserCollection(this.datastore.collection(Collections.USERS));
-        this.chatsCol = this.datastore.collection(Collections.CHATS);
-        this.messagesCol = this.datastore.collection(Collections.MESSAGES);
+  private constructor(private client: MongoClient) {}
+
+  /**
+   * Gets the MongoDatastore singleton
+   */
+  static async getInstance() {
+    if (!MongoDatastore.instance) {
+      // initialize new client
+      const client = new MongoClient(
+        process.env.MONGODB_CONNECTION_URI ?? 'mongodb://localhost:27017/'
+      )
+      try {
+        console.log(`Attempting to connect to ${process.env.MONGODB_CONNECTION_URI}`)
+        await client.connect()
+        console.log('Successfully connected to MongoDB!')
+        MongoDatastore.instance = new MongoDatastore(client)
+      } catch (exception) {
+        console.error('Something went wrong with MongoDB!')
+        console.error(exception)
+        await client.close()
+      }
     }
+    return MongoDatastore.instance
+  }
+}
 
-    static async getInstance() {
-        if (!MongoDatastore.instance) {
-            // initialize new client
-            let client = new MongoClient(process.env.MONGODB_CONNECTION_URI || "mongodb://localhost:27017/");
-            try {
-                console.log(`Attempting to connect to ${process.env.MONGODB_CONNECTION_URI}`)
-                client = await client.connect();
-
-                console.log("Successfully connected to MongoDB!");
-                MongoDatastore.instance = new MongoDatastore(client);
-            } catch (exception) {
-                console.error("Something went wrong with MongoDB!");
-                console.error(exception);
-                await client.close();
-            }
-        }
-        return MongoDatastore.instance;
-    }
-};
-
-export default MongoDatastore;
+// Consider using named exports
+export default MongoDatastore
--- src/datastore/UserCollection.ts
+++ src/datastore/UserCollection.ts
@@ -1,30 +1,30 @@
-import { Collection } from "mongodb";
-import User, { newUser } from "../models/User";
+import { Collection } from 'mongodb'
+import User, { newUser } from '../models/User'
 
 class UserCollection {
-    private col : Collection;
+  constructor(private col: Collection) {}
 
-    constructor(col: Collection) {
-        this.col = col;
+  async all(): Promise<User[]> {
+    return (await this.col.find({}).toArray()) as User[]
+  }
+
+  async register(email: string, username: string): Promise<User> {
+    // check valid input
+    // TODO: Validate email string either inside the component or do it here
+    // Do NOT try to use custom regex here, it will be incorrect
+    if (email.length === 0 || username.length === 0) {
+      throw Error('Email or username invalid!')
     }
 
-    async all(): Promise<User[]> {
-        return await this.col.find({}).toArray() as User[];
+    // check if user already exists
+    if (await this.col.findOne({ email })) {
+      throw Error('User already registered!');
     }
+    const insertedId = (await this.col.insertOne(newUser(email, username))).insertedId
 
-    async register(email: string, username: string): Promise<User> {
-        // check valid input
-        if (email.length == 0 || username.length == 0)
-            throw Error("Email or username invalid!");
+    // TODO: Validate schema instead of casting, or wrap this inside a getter
+    return (await this.col.findOne({ _id: insertedId })) as User
+  }
+}
 
-        // check if user already exists
-        if (await this.col.findOne({ email })) {
-            throw Error("User already registered!");
-        }
-        let insertedId = (await this.col.insertOne(newUser(email, username))).insertedId;
-
-        return await this.col.findOne({ "_id": insertedId }) as User;
-    }
-};
-
-export default UserCollection;
+export default UserCollection
