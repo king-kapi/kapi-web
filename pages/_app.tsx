7730fba5e162287a52b8a6de2bbe21e015932ef0
jackie-ou <jhgou@ucdavis.edu>
2023-02-08 16:57:54 -0800

    vertical nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Montserrat } from '@next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
