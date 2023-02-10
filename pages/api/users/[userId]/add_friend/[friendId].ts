4aea5d8b0a92df071422cd8431bc9f1c72f89c50
samuelili <samuelli4521@gmail.com>
2023-02-10 11:51:54 -0800

    added path param type for api routes

--- pages/api/users/[userId]/add_friend/[friendId].ts
+++ pages/api/users/[userId]/add_friend/[friendId].ts
@@ -2,15 +2,19 @@ import MongoDatastore from '@/src/datastore/MongoDatastore';
 import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+type AddFriendQueries = {
+  userId: string,
+  friendId: string
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
+    const { userId, friendId } = req.query as AddFriendQueries;
     const instance = await MongoDatastore.getInstance();
-    const userId = new ObjectId(req.query.userId as string);
-    const friendId = new ObjectId(req.query.friendId as string);
 
-    await instance.users.addFriend(userId, friendId);
+    await instance.users.addFriend(new ObjectId(userId), new ObjectId(friendId));
 
-    res.json(await instance.users.getUser(userId));
+    res.json(await instance.users.getUser(new ObjectId(userId)));
   } else {
     res.status(405).send("405 Method Not Allowed.");
   }
