109c912f51e009100d98f82d00eebb4c4bc4182f
jackie-ou <jhgou@ucdavis.edu>
2023-02-15 22:47:41 -0800

    resolve pr changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <SearchBar></SearchBar>
+        <SearchBar/>
       </main>
     </>
   );
