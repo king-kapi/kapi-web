dbc937ad05568b1de3f0ae6121bfcc77d23619c3
jackie-ou <jhgou@ucdavis.edu>
2023-02-16 01:47:31 -0800

    UI: Customize your dashboard page

---
+++ pages/CustomizeDashboard.tsx
@@ -0,0 +1,162 @@
+import styles from '@/styles/CustomizeDashboard.module.css';
+
+function CustomizeDashboard() {
+  return (
+    <div className={styles.body}>
+      <div className={styles.Container}>
+        <div className={styles.Banner}>
+          <div>
+            <h1 className={styles.Heading}>Customize your dashboard!</h1>
+            <h3 className={styles.Subheading}>
+              what are you interested in seeing on your home page?
+            </h3>
+          </div>
+          <button className={styles.Skip}>skip &gt;</button>
+        </div>
+        <div>
+          <ul className={styles.WidgetContainer}>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+            <li className={styles.Widget}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="163"
+                height="122"
+                viewBox="0 0 123 89"
+                fill="none"
+              >
+                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
+                <path
+                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
+                  stroke="#777777"
+                  stroke-width="2"
+                />
+              </svg>
+              <p className={styles.WidgetName}>Widget/Section</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <button className={styles.Done}>Done</button>
+    </div>
+  );
+}
+
+export { CustomizeDashboard };
