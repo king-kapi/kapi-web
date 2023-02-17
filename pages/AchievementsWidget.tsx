4a4fd7cce8abd5fef815d87608e21dae98f12d8d
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-02-17 13:13:00 -0800

    Achievements Widget

--- pages/AchievementsWidget.tsx
+++ pages/AchievementsWidget.tsx
@@ -1,9 +1,11 @@
-import exp from 'constants';
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Pagination, Grid} from 'swiper';
 import styles from '../styles/AchievementsWidget.module.css';
+import 'swiper/css/pagination';
+import 'swiper/css/grid';
 
-const achievements = [{}, {}, {}, {}, {}, {}, {}];
+const achievements = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
 
 function AchievementsWidget() {
   return (
@@ -12,11 +14,34 @@ function AchievementsWidget() {
         <h1>ACHIEVEMENTS</h1>
         <h2>looks like you're super cool</h2>
       </div>
-      <div className="swiper">
-        <div className="swiper-wrapper">
-          
-        </div>
-      </div>
+      <Swiper
+        modules={[Pagination, Grid]}
+        spaceBetween={30}
+        slidesPerView={3}
+        grid={{ rows: 2 }}
+        className={styles.mySwiper}
+        pagination={{ clickable: true}}
+      >
+        {achievements.map((x, index) => {
+          return (
+            <SwiperSlide key={index} className={styles.achievementSlide}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="69"
+                height="69"
+                viewBox="0 0 69 69"
+                fill="none"
+              >
+                <circle cx="34.5" cy="34.5" r="34" fill="#EAEAEA" stroke="#434343" />
+                <path
+                  d="M11.1289 11.127L57.8708 58.9818M57.8708 11.127L11.1289 58.9818"
+                  stroke="#434343"
+                />
+              </svg>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 }
