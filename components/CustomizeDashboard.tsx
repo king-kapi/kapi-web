bd304e2ff0a506f5fc8cc37a05bc793ab62646b9
samuelili <samuelli4521@gmail.com>
2023-02-18 18:45:00 -0800

    extracted svgs

--- components/CustomizeDashboard.tsx
+++ components/CustomizeDashboard.tsx
@@ -1,3 +1,4 @@
+import EmptyImageFrame from '@/assets/EmptyImageFrame';
 import styles from '@/styles/CustomizeDashboard.module.css';
 
 function CustomizeDashboard() {
@@ -16,139 +17,35 @@ function CustomizeDashboard() {
         <div>
           <ul className={styles.WidgetContainer}>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
             <li className={styles.Widget}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="163"
-                height="122"
-                viewBox="0 0 123 89"
-                fill="none"
-              >
-                <path d="M121.318 1.04297V87.3739H1.79883V1.04297H121.318Z" fill="#F0F0F0" />
-                <path
-                  d="M1.79883 1.04297L121.318 87.3739M1.79883 1.04297V87.3739M1.79883 1.04297H121.318M121.318 87.3739V1.04297M121.318 87.3739H1.79883M121.318 1.04297L1.79883 87.3739"
-                  stroke="#777777"
-                  strokeWidth="2"
-                />
-              </svg>
+              <EmptyImageFrame />
               <p className={styles.WidgetName}>Widget/Section</p>
             </li>
           </ul>
