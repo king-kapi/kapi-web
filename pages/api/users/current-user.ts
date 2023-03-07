cfcf400bfa08010411392f77b64e10cc4247be6b
samuelili <samuelli4521@gmail.com>
2023-03-07 13:31:56 -0800

    removed userId query in place of using session and added custom credentialsprovider for development use only in postman

--- pages/api/users/current-user.ts
+++ pages/api/users/current-user.ts
@@ -1,13 +1,15 @@
 import MongoDatastore from '@/src/datastore/MongoDatastore';
-import { ObjectId } from 'mongodb';
+import protectApiRoute from '@/src/utils/protectApiRoute';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { user } = await protectApiRoute(req, res);
+
     if (req.method === "GET") {
         const instance = await MongoDatastore.getInstance();
 
         // return list of users
-        res.status(200).json(await instance.users.getUser(new ObjectId(req.query.userId as string)));
+        res.status(200).json(await instance.users.getUser(user._id));
     } else {
         res.status(405).send("405 Method Not Allowed.");
     }
