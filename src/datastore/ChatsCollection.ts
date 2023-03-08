d2744f35ca88208dc43b80f5215075f19149f884
samuelili <samuelli4521@gmail.com>
2023-03-08 14:40:13 -0800

    added CRD for chats collection

--- src/datastore/ChatsCollection.ts
+++ src/datastore/ChatsCollection.ts
@@ -1,11 +1,34 @@
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import Chat from '../models/Chat';
+import Message from '../models/Message';
 
 class ChatsCollection {
   constructor(private col: Collection) { }
 
   async all(): Promise<Chat[]> {
-    return (await this.col.find({}).toArray()) as Chat[];
+    return await this.col.find({}).toArray() as Chat[];
+  }
+
+  async getMessages(chatId: ObjectId, count = 20): Promise<Message[]> {
+    return await this.col.find({
+      chatId
+    }).toArray() as Message[];
+  }
+
+  async getMessage(messageId: ObjectId): Promise<Message> {
+    return await this.col.findOne({ _id: messageId }) as Message;
+  }
+
+  async pushMessage(message: Message): Promise<ObjectId> {
+    const { insertedId } = await this.col.insertOne(message);
+
+    return insertedId;
+  }
+
+  async deleteMessage(messageId: Message): Promise<void> {
+    await this.col.deleteOne({
+      _id: messageId
+    });
   }
 }
 
