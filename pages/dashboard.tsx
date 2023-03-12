6cc6d87bd17630d48203c9ee28d1df9532b70fa6
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-03-11 20:45:37 -0800

    feat: click effect and placeholder event listener

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,8 +7,8 @@ import {
   createTheme,
   ThemeProvider,
   Card,
-  Box,
-  styled,
+  CardMedia,
+  CardActionArea,
 } from '@mui/material';
 
 // TODO: Both of the following types depends on what the designers want in these cards
@@ -46,24 +46,31 @@ const dashboardTheme = createTheme({
 
 function CustomizationChoice(props: ChoiceViewModel): JSX.Element {
   return (
-    <Card sx={{ textAlign: 'center' }}>
-      <Typography variant="body1">Some interest</Typography>
+    <Card sx={{ textAlign: 'center' }} onClick={() => console.log('click')}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
+        />
+        <Typography variant="body1">Some interest</Typography>
+      </CardActionArea>
     </Card>
   );
 }
 
 function CustomizeYourDashboard(): JSX.Element {
-  const mockInterests: Interest[] = new Array(20).fill({ name: 'bruh' });
+  // TODO: this is mock data, remove when api is ready
+  const mockInterests: Interest[] = new Array(8).fill({ name: 'bruh' });
   return (
     <ThemeProvider theme={dashboardTheme}>
       <Paper elevation={1} sx={{ p: 2 }}>
         <Stack direction="row" justifyContent="space-between" alignItems="center">
           <Typography variant="h4">Customize Your Dashboard!</Typography>
-          <Button variant="contained">Next {'>'}</Button>
+          <Button variant="text">Next {'>'}</Button>
         </Stack>
         <Typography variant="h5">What are you interested in seeing on your home page?</Typography>
 
-        <Box mt={2}>
+        <Stack spacing={2} direction="column" alignItems="center" mt={2}>
           <Grid container columns={20} spacing={2}>
             {
               // xs= 5, since grid has 20 width, 4 cards are displayed on each row
@@ -75,7 +82,11 @@ function CustomizeYourDashboard(): JSX.Element {
               ))
             }
           </Grid>
-        </Box>
+
+          <Button variant="contained" sx={{ width: 200 }} size="large">
+            <Typography>Done</Typography>
+          </Button>
+        </Stack>
       </Paper>
     </ThemeProvider>
   );
