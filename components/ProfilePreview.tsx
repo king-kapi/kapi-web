533d080b241a4b213629e9cee652dcbbb2f40f04
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-03-25 01:50:15 -0700

    feat: implemented Player Cards

--- components/ProfilePreview.tsx
+++ components/ProfilePreview.tsx
@@ -1,60 +1,34 @@
 import styles from '../styles/ProfilePreview.module.css';
-
+import ProfileCard from './ProfileCard';
 
 function ProfilePreview() {
   return (
     <div className={styles.ProfilePreviewContainer}>
       <div className={styles.TitleContainer}>
         <h1>Here are some buddies you can meet </h1>
-        <button className={styles.Reroll}>Reroll 
-        <svg className={styles.Vector} fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 576.469 576.469" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M275.762,95.253v38.955l116.228-67.104L275.762,0v39.283c-69.728,2.266-134.601,34.249-178.988,88.56l43.305,35.392 C173.798,121.976,222.893,97.493,275.762,95.253z"></path> <path d="M537.185,275.761c-2.268-69.73-34.25-134.602-88.562-178.989l-35.391,43.305c41.258,33.719,65.74,82.814,67.979,135.684 h-38.951l67.104,116.228l67.105-116.228H537.185z"></path> <path d="M300.706,481.211V442.26l-116.227,67.104l116.227,67.105v-39.285c69.73-2.268,134.604-34.25,178.988-88.562L436.39,413.23 C402.671,454.488,353.575,478.971,300.706,481.211z"></path> <path d="M95.255,300.705h38.953L67.106,184.477L0.001,300.705h39.284c2.267,69.729,34.249,134.604,88.56,178.988l35.392-43.305 C121.978,402.67,97.495,353.574,95.255,300.705z"></path> </g> </g> </g></svg></button>
+        <button className={styles.Reroll}>
+          Reroll
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="42"
+            height="42"
+            viewBox="0 0 42 42"
+            fill="none"
+            className={styles.Vector}
+          >
+            <path
+              d="M9.51561 31.8176C3.72291 25.5893 3.82535 15.8268 9.8434 9.765C12.202 7.37893 15.2688 5.81942 18.5862 5.31915L18.4095 0C13.7524 0.562337 9.42263 2.68442 6.12501 6.02085C-1.93918 14.1391 -2.03649 27.2488 5.81002 35.5515L1.35154 40.0383L15.462 40.8091L15.4235 25.8684L9.51561 31.8176ZM26.5377 1.19085L26.5762 16.1316L32.4841 10.185C38.2768 16.4184 38.1743 26.1809 32.1563 32.2376C29.7982 34.6242 26.7311 36.1838 23.4135 36.6834L23.5902 42C28.2473 41.437 32.5774 39.3161 35.8772 35.9817C43.9389 27.8583 44.0362 14.7487 36.1897 6.4511L40.6482 1.95915L26.5377 1.19085Z"
+              fill="black"
+            />
+          </svg>
+        </button>
       </div>
       <div className={styles.ServersContainer}>
-        <div className={styles.profileBox}>
-          <div className={styles.ProfilePicture}></div>
-          <h2 className={styles.UserName}>WoWPlayer123</h2>
-          <div className={styles.IconGroup}>
-          <div className={styles.DisplayIcon}>World of WarCraft</div>
-          <div className={styles.DisplayIcon}>League of Legends</div>
-          <div className={styles.DisplayIcon}>POC Gamer</div>
-          <div className={styles.DisplayIcon}>Non-Binary</div>
-          <div className={styles.DisplayIcon}>NA Region</div>
-          <div className={styles.DisplayIcon}>PST Time Zone</div>
-          </div>
-          <button className={styles.ProfileButton}>View Profile</button> 
-        </div>
-
-
-        <div className={styles.profileBox}>
-        <div className={styles.ProfilePicture}></div>
-          <h2 className={styles.UserName}>WoWPlayer123</h2>
-          <div className={styles.IconGroup}>
-          <div className={styles.DisplayIcon}>World of WarCraft</div>
-          <div className={styles.DisplayIcon}>League of Legends</div>
-          <div className={styles.DisplayIcon}>POC Gamer</div>
-          <div className={styles.DisplayIcon}>Non-Binary</div>
-          <div className={styles.DisplayIcon}>NA Region</div>
-          <div className={styles.DisplayIcon}>PST Time Zone</div>
-          </div>
-          <button className={styles.ProfileButton}>View Profile</button> 
-        </div>
-
-        <div className={styles.profileBox}>
-        <div className={styles.ProfilePicture}></div>
-          <h2 className={styles.UserName}>WoWPlayer123</h2>
-          <div className={styles.IconGroup}>
-            <div className={styles.DisplayIcon}>World of WarCraft</div>
-            <div className={styles.DisplayIcon}>League of Legends</div>
-            <div className={styles.DisplayIcon}>POC Gamer</div>
-            <div className={styles.DisplayIcon}>Non-Binary</div>
-            <div className={styles.DisplayIcon}>NA Region</div>
-            <div className={styles.DisplayIcon}>PST Time Zone</div>
-          </div>
-          <button className={styles.ProfileButton}>View Profile</button> 
-        </div>
-
-        </div>
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
       </div>
+    </div>
   );
 }
 
