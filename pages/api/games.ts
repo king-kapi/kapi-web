bb69c75c6bc4e8af089624a801c1af89afb9b091
samuelili <samuelli4521@gmail.com>
2023-03-28 01:28:25 -0700

    added endpoint for fetching games

---
+++ pages/api/games.ts
@@ -0,0 +1,18 @@
+import MongoDatastore from "@/src/datastore/MongoDatastore";
+import { GamesList } from "@/src/models/Game";
+import User from "@/src/models/User";
+import protectApiRoute from "@/src/utils/protectApiRoute";
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await protectApiRoute(req, res);
+
+    // const instance = await MongoDatastore.getInstance();
+
+    if (req.method === "GET") {
+        res.status(200).json(GamesList);
+    } else {
+        res.status(405).send("405 Method Not Allowed.");
+    }
+}
