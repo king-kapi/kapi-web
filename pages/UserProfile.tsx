a7657fcb5df5c47f28ad604354ca9d0643d92d4e
hungle546 <56492292+hungle546@users.noreply.github.com>
2023-03-29 20:57:57 -0700

    user page

--- pages/UserProfile.tsx
+++ pages/UserProfile.tsx
@@ -10,7 +10,7 @@ export default function UserProfile() {
                 <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 
                 <div className={styles.header2}>
-                    <h2> Personal Interest </h2>
+                    <h3> Personal Interest </h3>
                     <div className={styles.Tags}>
                         <h3 className={styles.Tag}>World of WarCraft</h3>
                         <h3 className={styles.Tag}>League of Legends</h3>
@@ -20,20 +20,32 @@ export default function UserProfile() {
                         <h3 className={styles.Tag}>PST - Time Zone</h3>
                     </div>
                 </div>
-                <h2 className={styles.header3}>Top Badges</h2>
+                <h3 className={styles.header3}>Top Badges</h3>
                 <div className={styles.badgeContainer}></div>
-                <h3 className={styles.badgeImage}></h3>
-                <h3 className={styles.badgeImage2}></h3>
-                <h3 className={styles.badgeImage3}></h3>
-                <h3 className={styles.badgeImage4}></h3>
-                <h4 className={styles.badgeNames}>50 Lobbies Joined</h4>
-                <h4 className={styles.badgeNames2}>Support God Top .3% Support in all games</h4>
-                <h4 className={styles.badgeNames3}>Friendly Buddy Top 10% Buddy who Joins Lobbies</h4>
-                <h4 className={styles.badgeNames4}>50 Lobbies Joined</h4>
+                <div className={styles.badgeImage} style={{top: 666 , left: 1047}}></div>
+                <div className={styles.badgeImage} style={{top: 666, left: 1317}} ></div>
+                <div className={styles.badgeImage} style={{top: 782, left: 1047}}></div>
+                <div className={styles.badgeImage} style={{top: 782, left: 1317}}></div>
+                <h4 className={styles.badgeNames} style={{top: 683 , left: 1156}}>50 Lobbies Joined</h4>
+                <h4 className={styles.badgeNames} style={{top: 683 , left: 1421}}>Support God Top .3% Support in all games</h4>
+                <h4 className={styles.badgeNames} style={{top: 793 , left: 1421}}>Friendly Buddy Top 10% Buddy who Joins Lobbies</h4>
+                <h4 className={styles.badgeNames} style={{top: 799 , left: 1156}}>50 Lobbies Joined</h4>
                 <div className={styles.line}></div>
+                <h1 className={styles.h4}>Communities</h1>
+                <div className={styles.communities} style={{top: 1025, left: 461}}></div>
+                <div className={styles.communities} style={{top: 1028, left: 871}}></div>
+                <div className={styles.communities} style={{top: 1028, left: 1257}}></div>
+
+                <h4 className={styles.socialHeader}>Social Link</h4>
+                <div className={styles.socialBadge} style={{top: 1362, left: 456}}></div>
+                <div className={styles.socialBadge} style={{top: 1362, left: 607}}></div>
+                <div className={styles.socialBadge} style={{top: 1362, left: 744}}></div>
+                
+                <div className={styles.requestButton}>
+                    <h4 className={styles.request}>Send Request</h4>
+                </div>
         </div>
     );
   }
-
   export { UserProfile };
   
