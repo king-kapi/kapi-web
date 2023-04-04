2fe41cd5c980e3f3e1ac4c8ae32419c90e2f7363
samuelili <samuelli4521@gmail.com>
2023-04-04 16:32:32 -0700

    moved socket logic over to express

--- server/index.ts
+++ server/index.ts
@@ -4,32 +4,35 @@ dotenv.config({
 });
 
 import next from 'next';
+import http from 'http';
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { NextServer } from 'next/dist/server/next';
-// import UserRoutes from './users/user-routes';
+import { Server as IOServer } from 'socket.io';
+import chatHandler from './chat';
 
-// prepare next app then start express server
+const nextApp: NextServer = next({ dev: process.env.NODE_ENV !== "production" });
+const handle = nextApp.getRequestHandler();
+
+const expressApp: Express = express();
+const server = http.createServer(expressApp);
+const port = process.env.PORT;
+
+const io = new IOServer(server);
+
+// middleware setup
+expressApp.use(bodyParser.json());
+
+// chat handler
+chatHandler(expressApp, io);
+
+// nextjs handler
+expressApp.all("*", (req: Request, res: Response) => {
+  return handle(req, res);
+}); 
+
 (async () => {
-  const app: NextServer = next({ dev: process.env.NODE_ENV !== "production" });
-  const handle = app.getRequestHandler();
-
-  console.log('Launching next server');
-  await app.prepare();
-
-  console.log('Staring express server')
-  const server: Express = express();
-  const port = process.env.PORT;
-
-  // middleware setup
-  server.use(bodyParser.json());
-
-  // UserRoutes(app);
-
-  // nextjs handler
-  server.all("*", (req: Request, res: Response) => {
-    return handle(req, res);
-  });
+  await nextApp.prepare();
 
   server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
