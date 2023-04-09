da3ca648e0c15ee99db388938dc7b761d7e33d1d
jackie-ou <jhgou@ucdavis.edu>
2023-04-08 21:00:51 -0700

    ui: games interest

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -7,7 +7,7 @@ function SideNav() {
   const [activeTab, setActiveTab] = useState(1); // TOOD: useRouter to get path name to check current tab
   const [onlineStatus, setOnlineStatus] = useState('1');
   const { data, isLoading, isError, error } = useQuery('userInfo', () =>
-    fetch(`http://localhost:3000/api/users/63f1762157a47ee3ab2337ae`).then(res => res.json())
+    fetch(`http://localhost:3000/api/user/current-user`).then(res => res.json())
   );
 
   const handleChange = (e: any) => {
@@ -42,6 +42,11 @@ function SideNav() {
         <ul>{friendsList}</ul>
       </div>
     );
+  }
+
+  function fetchUser() {
+    const user = data;
+    return <span className={styles.userName}>{`${user.username}#${user.tag}`}</span>;
   }
 
   if (isLoading) {
@@ -233,7 +238,7 @@ function SideNav() {
             <div className={getCurrentStatus()}></div>
           </div>
           <div className={styles.userNameContainer}>
-            <span className={styles.userName}>{`${data.username}#${data.tag}`}</span>
+            {fetchUser()}
             <select
               name="status"
               className={styles.status}
