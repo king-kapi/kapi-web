696d6d47062730958c0e5c4a4b69834f7f4f412c
samuelili <samuelli4521@gmail.com>
2023-04-29 23:01:41 -0700

    changed state to use an enum

--- components/InterestMatch.tsx
+++ components/InterestMatch.tsx
@@ -1,12 +1,19 @@
-import styles from '../styles/InterestMatch.module.css';
-import { useState } from 'react';
+import styles from "../styles/InterestMatch.module.css";
+import { useState } from "react";
+
+enum FindBuddyStates {
+  NOT_SELECTED = -1,
+  NO_PREFERENCE = 0,
+  PREFERENCE = 1
+}
 
 export default function InterestMatch() {
-    const [value, setValue] = useState('1');
+  const [findBuddy, setFindBuddy] = useState<FindBuddyStates>(FindBuddyStates.NOT_SELECTED);
 
-    const handleChange = (e:any) => {
-      setValue(e.target.value);
-    };
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setFindBuddy(Number(e.currentTarget.value));
+  };
+
   return (
     <div className={styles.InterestMatchContainer}>
       <h1 className={styles.Header}>Lets find a gaming buddy for you</h1>
@@ -16,17 +23,17 @@ export default function InterestMatch() {
       <h2 className={styles.Question}>
         2. Do you want to be matched with someone based on your profile?
       </h2>
-      <h2 className={styles.SubHeader} style={{ marginLeft: '1.5rem' }}>
+      <h2 className={styles.SubHeader} style={{ marginLeft: "1.5rem" }}>
         i.e, age, gender and sexual identity, ethnography, etc.
       </h2>
       <div className={styles.Options}>
         <label className={styles.Option}>
-          <input type="radio" value='1' checked={value === '1'} onChange={handleChange}/>
+          <input type="radio" value={FindBuddyStates.PREFERENCE} checked={findBuddy === FindBuddyStates.PREFERENCE} onChange={handleChange} />
           Yes, I would like to find a gaming buddy based on similar interest
           <br />
         </label>
         <label className={styles.Option}>
-          <input type="radio" value='0' checked={value === '0'} onChange={handleChange}/>
+          <input type="radio" value={FindBuddyStates.NO_PREFERENCE} checked={findBuddy === FindBuddyStates.NO_PREFERENCE} onChange={handleChange} />
           No, it does not matter based on my profile
         </label>
       </div>
