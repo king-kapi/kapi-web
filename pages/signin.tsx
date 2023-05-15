1409c22bf35d6aea5901e214533c78895b07df85
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-05-14 19:16:46 -0700

    sign in page revamped

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,8 @@
-import { Icon } from '@iconify/react';
+import Icon, {Icons} from '@/components/Icon';
 import { signIn } from 'next-auth/react';
 import styles from '../styles/SignInPage.module.css';
+import EmptyLayout from '@/components/layouts/EmptyLayout';
+import Button from '@/components/Button';
 
 export default function SignInPage() {
     // console.log(providers)
@@ -8,28 +10,30 @@ export default function SignInPage() {
     return (
         <div className={styles.SignInPage}>
             <div className={styles.Logo}></div>
-            <div className={styles.SignInContainer}>
+            <div className={[styles.SignInContainer, 'bg-black'].join(' ')}>
                 <div className={styles.SignInHeader}>
                     <h1>Welcome</h1>
                     <h3>Discover safe and inclusive gaming communities for you</h3>
                 </div>
                 <div className={styles.SignInButtons}>
-                    <button onClick={async () => {
+                    <Button className={styles.Button} type='secondary' onClick={async () => {
                         console.log(signIn('google', { callbackUrl: '/' }))
                     }}>
-                        <Icon icon='logos:google-icon' />
+                        <Icon icon={Icons['GOOGLE_ICON']} />
                         Sign in with Google
-                    </button>
-                    <button onClick={() => signIn('discord', { callbackUrl: '/' })}>
-                        <Icon icon='logos:discord-icon' />
+                    </Button>
+                    <Button className={styles.Button} onClick={() => signIn('discord', { callbackUrl: '/' })}>
+                        <Icon icon={Icons["DISCORD_ICON"]} />
                         Sign in with Discord
-                    </button>
+                    </Button>
                 </div>
                 <div className={styles.SignInDivider}>
-                    <div className={styles.Line}><span>or</span></div>
+                    <div className={styles.Line}><span className='bg-black'>or</span></div>
                 </div>
-                <a className={styles.CreateAccount}>Create an Account</a>
+                <Button type='secondary' className={styles.CreateAccount}>Create an Account</Button>
             </div>
         </div>
     )
 }
+
+SignInPage.getLayout = EmptyLayout.getLayout
