63f15d2d3ce24a0cc7588584b2cd41a5e58bd9d3
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2023-08-28 22:12:53 -0700

    functionality to "find a buddy"  in party finder

--- pages/party-finder/index.tsx
+++ pages/party-finder/index.tsx
@@ -6,6 +6,9 @@ import Button from '@/src/components/Button';
 import { useAtom } from 'jotai';
 import { partyFinderAtom } from '@/src/atoms/partyFinderAtom';
 import Link from 'next/link';
+import InterestMatch from '@/src/components/InterestMatch';
+import TimeZone from '@/src/components/TimeZone';
+import InvitationMessage from '@/src/components/InvitationMessage';
 
 export default function PartyFinder() {
   const [pageNumber, setPageNumber] = useState(1);
@@ -14,12 +17,15 @@ export default function PartyFinder() {
   return (
     <div className="w-[59.375rem] h-[53.125rem] border-solid border p-16 relative left-[19%] top-[8%] rounded-[.625rem]">
       <div className="flex justify-between mb-[2.62rem]">
-        <h1 className="text-2xl font-medium">Question {pageNumber} of 3</h1>
+        <h1 className="text-2xl font-medium">Question {pageNumber}</h1>
         <h1 className="text-pink-500 text-2xl font-medium">Required</h1>
       </div>
       {pageNumber === 1 && <PartyOptions />}
       {pageNumber === 2 && <HonorOfConduct />}
       {pageNumber === 3 && <GameSelect />}
+      {pageNumber === 4 && <InterestMatch />}
+      {pageNumber === 5 && <TimeZone />}
+      {pageNumber === 6 && <InvitationMessage />}
       <Button
         buttonType="secondary"
         className="w-[7.5rem] absolute bottom-16"
@@ -31,16 +37,37 @@ export default function PartyFinder() {
       </Button>
       <Link
         href={
-          survey.honorConduct === true && survey.games?.length !== 0 && pageNumber === 3
+          survey.honorConduct === true &&
+          survey.games?.length !== 0 &&
+          ((pageNumber === 3 && survey.partyOption === '/party-finder/LobbyFinder') ||
+            (pageNumber === 6 && survey.partyOption === '/party-finder/BuddyFinder'))
             ? survey.partyOption + ''
             : ''
         }
       >
         <Button
-          buttonType="primary"
+          buttonType={
+            (pageNumber === 1 && survey.partyOption) ||
+            (pageNumber === 2 && survey.honorConduct) ||
+            (pageNumber === 3 && survey.games?.length != 0) ||
+            (pageNumber === 4 && survey.interestMatch != null) ||
+            pageNumber === 5 ||
+            pageNumber === 6
+              ? 'primary'
+              : 'secondary'
+          }
           className="w-[7.5rem] absolute bottom-16 right-16"
           onClick={() => {
-            if (pageNumber != 3) setPageNumber(pageNumber + 1);
+            if (
+              pageNumber != 6 &&
+              ((pageNumber === 1 && survey.partyOption) ||
+                (pageNumber === 2 && survey.honorConduct) ||
+                (pageNumber === 3 && survey.games?.length != 0) ||
+                (pageNumber === 4 && survey.interestMatch != null) ||
+                pageNumber === 5 ||
+                pageNumber === 6)
+            )
+              setPageNumber(pageNumber + 1);
           }}
         >
           Next
--- src/components/InvitationMessage.tsx
+++ src/components/InvitationMessage.tsx
@@ -1,9 +1,11 @@
-import { useContext, useEffect, useState } from "react";
-import styles from "@/src/styles/InvitationMessage.module.css";
+import { useContext, useEffect, useState } from 'react';
+import styles from '@/src/styles/InvitationMessage.module.css';
+import { partyFinderAtom } from '../atoms/partyFinderAtom';
+import { useAtom } from 'jotai';
 
 export default function InvitationMessage() {
-  const [message, setMessage] = useState("");
-
+  const [survey, setSurvey] = useAtom(partyFinderAtom);
+  const [message, setMessage] = useState(survey.message);
 
   useEffect(() => {
     // setContent({ ...content, message: message });
@@ -16,13 +18,18 @@ export default function InvitationMessage() {
         <h3 className={styles.SubHeader}>
           We want to ask you a few questions to find people based on your preferences.
         </h3>
-        <h2 className={styles.Question}>3. Edit Invitation Message? (Optional)</h2>
+        <h2 className={styles.Question}>Edit Invitation Message? (Optional)</h2>
       </div>
       <textarea
-        className={[styles.TextBox, "bg-mediumGrey placeholder-textColor"].join(" ")}
-        onChange={e => setMessage(e.target.value)}
+        className={[styles.TextBox, 'bg-mediumGrey placeholder-textColor'].join(' ')}
+        onChange={e => {
+          setMessage(e.target.value);
+          setSurvey({ ...survey, message: message });
+        }}
         placeholder="Hi ____, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      ></textarea>
+      >
+        {message}
+      </textarea>
     </div>
   );
 }
